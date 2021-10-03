957ab1bb4604cfb9db56c06149d9f44f816abb6d
damszew <damian.szewczyk111@gmail.com>
2021-10-03 16:47:47 +0000

    Add test case for 2nd user story

--- src/lib.rs
+++ src/lib.rs
@@ -37,4 +37,30 @@ mod should {
 
         assert_eq!(result, vec![employees[1].clone(), employees[3].clone()]);
     }
+
+    #[test]
+    fn return_epmloyees_sorted_by_names() {
+        let employees = vec![
+            Employee {
+                name: "Max".into(),
+                age: 17,
+            },
+            Employee {
+                name: "Sepp".into(),
+                age: 18,
+            },
+            Employee {
+                name: "Nina".into(),
+                age: 15,
+            },
+            Employee {
+                name: "Mike".into(),
+                age: 51,
+            },
+        ];
+
+        let result = staff_system(employees.clone());
+
+        assert_eq!(result, vec![employees[3].clone(), employees[1].clone()]);
+    }
 }
