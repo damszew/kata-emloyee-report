f7943dc2d0832828ac7705390bcdb87015bf85f3
damszew <damian.szewczyk111@gmail.com>
2021-10-03 16:44:44 +0000

    First failing test

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,40 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Employee {
+    pub name: String,
+    pub age: u32,
+}
+
+pub fn staff_system(_employees: Vec<Employee>) -> Vec<Employee> {
+    todo!()
+}
+
 #[cfg(test)]
-mod tests {
+mod should {
     use super::*;
 
     #[test]
-    fn test_name() {
-        assert!(true)
+    fn return_epmloyees_olter_than_18() {
+        let employees = vec![
+            Employee {
+                name: "Max".into(),
+                age: 17,
+            },
+            Employee {
+                name: "Sepp".into(),
+                age: 18,
+            },
+            Employee {
+                name: "Nina".into(),
+                age: 15,
+            },
+            Employee {
+                name: "Mike".into(),
+                age: 51,
+            },
+        ];
+
+        let result = staff_system(employees.clone());
+
+        assert_eq!(result, vec![employees[1].clone(), employees[3].clone()]);
     }
 }
